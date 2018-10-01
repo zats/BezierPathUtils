50828a3805fa9548aa6d4fadb5545ad01da9ce1b
Sash Zats <sash@zats.io>
2018-09-30 20:10:39 -0400

    Initial Commit

---
+++ BezierPathUtils/BezierPathUtils.h
@@ -0,0 +1,19 @@
+//
+//  BezierPathUtils.h
+//  BezierPathUtils
+//
+//  Created by Sash Zats on 9/30/18.
+//  Copyright © 2018 Sash Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BezierPathUtils.
+FOUNDATION_EXPORT double BezierPathUtilsVersionNumber;
+
+//! Project version string for BezierPathUtils.
+FOUNDATION_EXPORT const unsigned char BezierPathUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BezierPathUtils/PublicHeader.h>
+
+
